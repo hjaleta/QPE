00df4b184c561b955e687e4eca3f602bedb1f4cd
hjaleta <hjaleta@gmail.com>
2022-02-02 22:54:51 +0100

    vrs 9

---
+++ setup.py
@@ -0,0 +1,13 @@
+# from setuptools import setup
+
+# setup(
+#     name='QPE',
+#     verions='0.0.1.9',
+#     author='Hjalmar Lindstedt, Riccardo Conte, Miguel Carrera Belo',
+#     author_email='hjaleta@gmail.com',
+#     description='Qiskit applications for Quantum Phase Estimation',
+#     # license='GPL',
+#     url='https://github.com/hjaleta/QPE',
+#     packages=['QPE', 'QPE.Circuits'],
+    
+#     )
